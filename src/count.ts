005616f6eb8b089caeabf4b4a008151dcb63b788
john-de-graaf <gophers26@gmail.com>
2026-04-09 20:33:33 +0200

    Delete src directory

--- src/count.ts
+++
@@ -1,9 +0,0 @@
-import { QUESTIONS } from './data';
-
-const counts: Record<string, number> = {};
-
-QUESTIONS.forEach(q => {
-  counts[q.themeId] = (counts[q.themeId] || 0) + 1;
-});
-
-console.log(counts);
